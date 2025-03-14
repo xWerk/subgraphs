1101a285ad06e789fe4496d941553f2803530927
gabrielstoica <marius_gabriel1998@yahoo.com>
2025-03-14 10:32:30 +0200

    feat: cache Spaces addresses to reduce initial deployment time

--- apps/envio/src/utils/get-space-addresses.ts
+++ apps/envio/src/utils/get-space-addresses.ts
@@ -1,14 +1,13 @@
 import { handlerContext } from "generated";
 
 const endpoint = `${process.env.WERK_API_ENDPOINT}/space/get-addresses`;
+let cachedAddresses: Array<string> = [];
 
 async function fetchFromEndpoint(context: handlerContext): Promise<Array<string> | null> {
   try {
     const response = await fetch(`${endpoint}`);
     if (response.ok) {
       const addresses: any = await response.json();
-      context.log.info(addresses[0]);
-      context.log.info(addresses.length);
       return addresses;
     } else {
       throw new Error("Unable to fetch from endpoint");
@@ -20,9 +19,15 @@ async function fetchFromEndpoint(context: handlerContext): Promise<Array<string>
 }
 
 export async function tryFetchFromEndpoint(context: handlerContext): Promise<Array<string>> {
+  if (cachedAddresses.length > 0) {
+    return cachedAddresses;
+  }
+
   const addresses = await fetchFromEndpoint(context);
   if (addresses && addresses.length > 1) {
+    cachedAddresses = addresses;
     return addresses;
   }
+
   return [];
 }
