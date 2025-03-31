f5db53615f664d01713e0fe93b030dfdd7f12999
gabrielstoica <marius_gabriel1998@yahoo.com>
2025-03-31 13:07:02 +0300

    refactor(space-created): store Space addresses in the 'StationRegistry_Spaces' table

--- apps/envio/constants/index.ts
+++ apps/envio/constants/index.ts
@@ -1,1 +1,4 @@
 export const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
+
+// Address of the {StationRegistry} contract deployed at deterministic address across chains
+export const STATION_REGISTRY_ADDRESS = "0xe1026Ab8272746dc93Efcd1423355F3b23E3e0aB";
--- apps/envio/src/space/transfer.ts
+++ apps/envio/src/space/transfer.ts
@@ -1,11 +1,16 @@
-import { Space_Transfer, USDC } from "generated";
-import { getSpaceAddresses } from "../utils/get-space-addresses";
 import { formatUnits } from "viem";
+import { Space_Transfer, USDC } from "generated";
+import { STATION_REGISTRY_ADDRESS } from "../../constants";
 
 USDC.Transfer.handler(async ({ event, context }) => {
-  const spaceAddresses = await getSpaceAddresses(context);
+  // Retrieve the {StationRegistry_Spaces} context entity
+  const stationRegistry = await context.StationRegistry_Spaces.get(`${event.chainId}_${STATION_REGISTRY_ADDRESS}`);
 
-  if (spaceAddresses.includes(event.params.from) || spaceAddresses.includes(event.params.to)) {
+  // Get the Space deployed on the current chain ID through the `StationRegistry` factory contract
+  const spaceAddresses = stationRegistry?.spaces;
+
+  // Filter and store only the USDC transfers that involve a Space address
+  if (spaceAddresses && (spaceAddresses.includes(event.params.from) || spaceAddresses.includes(event.params.to))) {
     const entity: Space_Transfer = {
       id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
       timestamp: BigInt(event.block.timestamp),
