c54c3ba304327392dfce806acd2c6e03e8994b20
gabrielstoica <marius_gabriel1998@yahoo.com>
2025-03-31 16:42:59 +0300

    perf: use loaders to handle USDC transfers

--- apps/envio/src/space/transfer.ts
+++ apps/envio/src/space/transfer.ts
@@ -1,27 +1,34 @@
 import { formatUnits } from "viem";
 import { Space_Transfer, USDC } from "generated";
-import { STATION_REGISTRY_ADDRESS } from "../../constants";
+import { STATION_REGISTRY_ADDRESS, ZERO_ADDRESS } from "../../constants";
 
-USDC.Transfer.handler(async ({ event, context }) => {
-  // Retrieve the {StationRegistry_Spaces} context entity
-  const stationRegistry = await context.StationRegistry_Spaces.get(`${event.chainId}_${STATION_REGISTRY_ADDRESS}`);
+USDC.Transfer.handlerWithLoader({
+  loader: async ({ event, context }) => {
+    // Retrieve the {StationRegistry_Spaces} context entity
+    const stationRegistry = await context.StationRegistry_Spaces.get(`${event.chainId}_${STATION_REGISTRY_ADDRESS}`);
 
-  // Get the Space deployed on the current chain ID through the `StationRegistry` factory contract
-  const spaceAddresses = stationRegistry?.spaces;
+    // Get the Space deployed on the current chain ID through the `StationRegistry` factory contract
+    const spaceAddresses = stationRegistry?.spaces;
 
-  // Filter and store only the USDC transfers that involve a Space address
-  if (spaceAddresses && (spaceAddresses.includes(event.params.from) || spaceAddresses.includes(event.params.to))) {
-    const entity: Space_Transfer = {
-      id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
-      timestamp: BigInt(event.block.timestamp),
-      chainId: event.chainId.toString(),
-      asset: event.srcAddress,
-      from: event.params.from,
-      to: event.params.to,
-      value: formatUnits(event.params.value, 6),
-      hash: event.transaction.hash,
-    };
+    return { spaceAddresses };
+  },
+  handler: async ({ event, context, loaderReturn }) => {
+    const { spaceAddresses } = loaderReturn;
 
-    context.Space_Transfer.set(entity);
-  }
+    // Filter and store only the USDC transfers that involve a Space address
+    if (spaceAddresses && (spaceAddresses.includes(event.params.from) || spaceAddresses.includes(event.params.to))) {
+      const entity: Space_Transfer = {
+        id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+        timestamp: BigInt(event.block.timestamp),
+        chainId: event.chainId.toString(),
+        asset: event.srcAddress,
+        from: event.params.from,
+        to: event.params.to,
+        value: formatUnits(event.params.value, 6),
+        hash: event.transaction.hash,
+      };
+
+      context.Space_Transfer.set(entity);
+    }
+  },
 });
