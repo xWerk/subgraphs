0afba54c4fd18ca2197ba4ae2f38157799e1523c
gabrielstoica <marius_gabriel1998@yahoo.com>
2026-02-11 10:58:45 +0200

    refactor: StationRegistry 'SpaceCreated' event, remove outdated events and update deployment address

--- apps/envio/src/station-registry/space-created.ts
+++ apps/envio/src/station-registry/space-created.ts
@@ -3,8 +3,7 @@ import { StationRegistry, StationRegistry_SpaceCreated } from "generated";
 StationRegistry.SpaceCreated.handler(async ({ event, context }) => {
   const entity: StationRegistry_SpaceCreated = {
     id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
-    owner: event.params.owner,
-    stationId: event.params.stationId,
+    admin: event.params.admin,
     space: event.params.space,
   };
 
